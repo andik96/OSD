336c24eb1f118d94219e88aee2747aebb9372620
Andreas Kemperle <andreas.kemperle@edu.fh-joanneum.at>
2018-05-27 11:45:55 +0200

    Started with testing Io Classes

--- TheReactionGame/TestReactionGame/PlayerTest.cpp
+++ TheReactionGame/TestReactionGame/PlayerTest.cpp
@@ -1,8 +1,36 @@
+
+/********************************
+#   THE REACTION GAME           #
+#   PlayerTest.cpp              #
+#                               #
+#   Authors:                    #
+#       KEMPERLE Andreas        #
+#       WINKLER  Andreas        #
+#                               #
+#   created: 2018/05/23         #
+#   Version: 2018/05/27 - V1.1  #
+********************************/
+
+
+// ===============================================================
+// INCLUDES
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "../TheReactionGame/Player.hpp"
 
+
+// ===============================================================
+// NAMESPACES
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+// #################################### SECTION BREAK ####################################
+
+
+// ===============================================================
+// REACTION GAME TESTS
 
 namespace TestReactionGame
 {		
@@ -17,7 +45,7 @@ namespace TestReactionGame
 			Assert::AreEqual(3, static_cast<int>(p.read_button_pin()));
 			Assert::AreEqual(5, static_cast<int>(p.read_led_pin()));
 			Assert::AreEqual("AndiK", p.read_name().c_str(), "Names are different.");
-			//Assert::AreEqual("0", static_cast<std::string>(p.read_wins()), "wins are different");
+			Assert::AreEqual(0, static_cast<int>(p.read_wins()) );
 		}
 
 	};
